3debce304c90da7688bfa09bbda86a792fee9b02
dragon10 <40717349+lonngxiang@users.noreply.github.com>
2023-10-18 07:30:16 +0800

    Update configs.py

--- configs.py
+++ configs.py
@@ -6,5 +6,5 @@ vector_store = None
 
 
 ## model address
-embedding_model_address = "" ## "shibing624/text2vec-base-chinese"
+embedding_model_address = "" ## "shibing624/text2vec-base-chinese",local address where the model is saved
 llm_service_url_address = "" ## fschat address, http://*****:21002
